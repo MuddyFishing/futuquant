c2a459b8bfe8efec97e13a22a272eadf606ffee2
ysq <ysq@corp.futu5.com>
2017-11-28 16:29:55 +0800

    调试， 添加注释说明

--- futuquant/examples/common/get_trade_status.py
+++ futuquant/examples/common/get_trade_status.py
@@ -7,43 +7,48 @@
 from futuquant.open_context import *
 from datetime import datetime, timedelta
 
+# 指定日期类型
+ALL_DAYS = 'Alldays'                # 所有的自然天
+TRADE_DAYS = 'Tradedays'            # 仅交易日
 
-ALL_DAYS = 'Alldays'
-TRADE_DAYS = 'Tradedays'
+# 股票交易状态 :
+STATUS_NONE = ''                    # 非交易日
+STATUS_TRADEING= 'trading'          # 交易日
+STATUS_SUPENSION = 'suspension'     # 股票停牌
 
 
-STATUS_NONE = ''
-STATUS_TRADEING= 'trading'
-STATUS_SUPENSION = 'suspension'
-
-
-def get_trade_status(quote_context=None, strcode='HK.00700', start='2016-01-01', end='2017-12-30', days=TRADE_DAYS):
+def get_trade_status(quote_context=None, strcode='HK.00700', start='2016-01-01', end='2017-12-30', days=ALL_DAYS):
     '''
-    :param quote_context: api quote context
-    :param strcode:
+    :param quote_context: api行情对象
+    :param strcode: 股票code
     :param start: 开始日期
     :param end: 结束日期
-    :param days: 返回的日期类型 ALL_DAYS （所有的） / TRADE_DAYS (交易日)
-    :return: pd.dataframe  - 表头 “DateTime” / “Trade_status”
+    :param days: 返回的日期类型 见文件头定义 ALL_DAYS （所有的） / TRADE_DAYS (交易日)
+    :return: (ret, data)  ret == 0  data为 pd.dataframe数据 表头为 “DateTime”  “Trade_status”
+                          ret != 0  data为错误字符串
     '''
     if not quote_context:
         raise Exception("get_trade_status - quote_context param error!")
 
+    # 获取股票市场
     ret_code, content = split_stock_str(strcode)
     if ret_code != RET_OK:
         raise Exception("get_trade_status - strcode param error!")
     market_val, _ = content
 
+    # 得到该市场的所有交易日
     ret, data_trade = quote_context.get_trading_days(TRADE.REV_MKT_MAP[market_val], start, end)
     if ret != 0:
         return ret, str(data_trade)
     data_trade = sorted(data_trade)
     dict_trade = {x: True for x in data_trade}
 
+    # 得到指定时间内，股票的停牌日
     ret, data_sup = quote_context.get_suspension_info(strcode, start, end)
     if ret != 0:
         return ret, str(data_sup)
 
+    # 停牌数据转成list , dict, 以便后续计算
     str_dates = ''
     for ix, row in data_sup.iterrows():
         str_dates = row['suspension_dates']
@@ -51,8 +56,8 @@ def get_trade_status(quote_context=None, strcode='HK.00700', start='2016-01-01',
     list_sup = str_dates.split(',')
     dict_sup = {x: True for x in list_sup}
 
+    # 依据参数计算返回股票各天的交易状态
     ret_status = []
-
     if days == TRADE_DAYS:
         for x in data_trade:
             ret_status.append({'DateTime': x, 'Trade_status': STATUS_SUPENSION if (x in dict_sup) else STATUS_TRADEING})
@@ -75,6 +80,7 @@ def get_trade_status(quote_context=None, strcode='HK.00700', start='2016-01-01',
     else:
         raise Exception("get_trade_status - days param error!")
 
+    # 组装返回数据为dataframe数据
     col_list = ['DateTime', 'Trade_status']
     pd_frame = pd.DataFrame(ret_status, columns=col_list)
 
