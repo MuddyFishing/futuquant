82d533fcfd704eaadd2b9997bd36eea2ab8d9dca
pattiluo <pengtingluo@gmail.com>
2018-08-14 14:08:57 +0800

    change config

--- futuquant/examples/app/stock_alarm/Config.py
+++ futuquant/examples/app/stock_alarm/Config.py
@@ -2,14 +2,11 @@ import requests
 class Config:
     def __init__(self):
         # wechat
-        self.appid = 'wx7a25fbe5fc90799a'       # AppID
-        self.secrect = 'f34624551ea8f5f784f2622378fe91f8'      # Secret
-        # self.appid = 'wxbe3ec6c53ff67a31'
-        # self.secrect = 'd8f141fe58642381bac82a6f0102ca10'
+        self.appid = ''       # AppID
+        self.secrect = ''      # Secret
 
         # test_user_list
-        self.test_user_list = {#'oaeaj0xqRf_m3l1Ln4FQ8rdoAvHc',  # ysq
-                          #'oaeaj02eXJu9OuaiBtaHjNnvzv-Y',  # 享阝木木¹³¹¹ºº⁷⁷⁵⁵º
+        self.test_user_list = {
                           'oaeaj02DzklyZHavotk2X3mt6JuA'  # lpt
                           }
 
@@ -17,7 +14,7 @@ class Config:
         self.test_user_nickname = {'lpt'}
 
         # wechat token
-        self.token = 'yundun999'  # token
+        self.token = ''  # token
 
         # parameter: 越价率
         self.premium_rate = 0.005
@@ -26,7 +23,7 @@ class Config:
         self.warning_limit = 5
 
         # template_id
-        self.template_id = "jr67sFJ5w4ln_ty6e0BHSBLZNOUOgMXOC-ph9u6xWwQ"
+        self.template_id = ""
 
         # mysql
         # self.host = '127.0.0.1'
