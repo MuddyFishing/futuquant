3f746a4826092b1c63f6f8a3898bdc208604b7cb
winniewang <winniewang@futunn.com>
2018-08-29 09:55:02 +0800

    update test cases

--- futuquant/testcase/person/winnie/quote/test_get_market_snapshot.py
+++ futuquant/testcase/person/winnie/quote/test_get_market_snapshot.py
@@ -12,9 +12,9 @@ def test_get_market_snapshot():
     code = list(ret_data['code'])
     del code[200:]
     # print(ret_data['code'])
-    code_list=['HK.00700', 'HK.00066', 'US.AAPL', 'SZ.000700']
-    # code_list = ['HK.999010', 'HK.22170', 'US.AAPL180921P235000', 'US.AAPL180921C135000']  # 'US_OPTION.AAPL180921C225000'
-    print(quote_ctx.get_market_snapshot(['HK_FUTURE.999010', 'HK_FUTURE.999011']))
+    # code_list = ['HK.00700', 'HK.00066', 'US.AAPL', 'SZ.000700']
+    code_list = ['US.AAPL180921P235000', 'US.AAPL180921C135000']  # 'US_OPTION.AAPL180921C225000'
+    print(quote_ctx.get_market_snapshot(code_list))
 
 
 if __name__ == '__main__':
