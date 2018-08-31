7dcfe7c05420151b9f35ce5dce73cdd219b3c5ce
alexqli <alexqli@futunn.com>
2018-08-31 17:17:15 +0800

    修复运行时错误

--- futuquant/common/network_manager.py
+++ futuquant/common/network_manager.py
@@ -114,7 +114,7 @@ class NetManager:
             self._is_polling = True
 
             now = datetime.now()
-            events = self._selector.select(0.02)
+            events = self._selector.select(0)
             for key, evt_mask in events:
                 conn = key.data
                 if evt_mask & selectors.EVENT_WRITE != 0:
@@ -167,15 +167,13 @@ class NetManager:
         :type now: datetime
         :return:
         """
-        timeout_req_set = set()
-        for proto_info, req_time in dict(conn.req_dict.items()):  # type: ProtoInfo, datetime
+        req_dict = dict(conn.req_dict.items())
+        for proto_info, req_time in req_dict.items():  # type: ProtoInfo, datetime
             elapsed_time = now - req_time
             if elapsed_time.total_seconds() >= self._sync_req_timeout:
                 self._on_packet(conn, proto_info._asdict(), Err.Timeout.code, Err.Timeout.text, None)
-                timeout_req_set.add(proto_info)
+                del conn.req_dict[proto_info]
 
-        for proto_info in timeout_req_set:
-            del conn.req_dict[proto_info]
 
     def _thread_func(self):
         while True:
@@ -354,7 +352,7 @@ class NetManager:
                     sync_req_evt = conn.sync_req_evt
                     self.send(conn_id, proto_info, req_str)
                     break
-            sleep(0.01)
+            sleep(0)
 
         sync_req_evt.wait()
         sync_req_evt.clear()
